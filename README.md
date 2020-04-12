cb1131f236c4cd5640936d95604c31d544b8d062
Xandy Liu <liuxingyang.xandy@hotmail.com>
2020-04-11 20:28:26 -0400

    added lib

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Created for:
 Georgia Tech ECE4180-A Embedded Systems Design - Final Project
 
 NOTE FOR RASPBERRY PI 3: The Raspberry pi 3 has changed things around a bit: ttyAMA0 now refers to the serial port that is connected to the bluetooth. The old serial port is now called ttyS0. So if you have an RPI3, everywhere you see "ttyAMA0" below, you should read "ttyS0".
+
+
+_rst = 1;           ->      gpioWrite(_rst, 1); 
+_cmd.putc(c);       ->      serWriteByte(_cmd, (unsigned) c);
+_cmd.getc();        ->      int = serReadByte(_cmd);
+wait_ms(5);         ->      time_sleep(0.005);
+wait(3);            ->      time_sleep(3);
+wait_us(500);       ->      time_sleep(0.0005);
+_cmd.readable()     ->      serDataAvailable(_cmd) != 0
--- test_xandy.cpp
+++ test_xandy.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
 #include <pigpio.h> // include GPIO library
 #include <signal.h> // needed to clean up CTL C abort
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "uLCD_4DGL.h"
+
 #define TXD 14 // TXD pin is GPIO_14
 #define RXD 15 // RXD pin is GPIO_15
+#define RST 18 // RST pin is GPIO_18
 
 // Called when CTL C or STOP button hit
 static void err_handler (int sig){
@@ -30,26 +32,20 @@ int main(int argc, char *argv[])
    atexit(exit_handler);  // exit handler cleanup 
 
    //IO code starts here
-   char *devtty = (char *)"/dev/ttyS0";
+   char *devtty = (char *)"/dev/ttyS0"; // Pi3: ttyS0; older Pis: ttyAMA0
    gpioSetMode(TXD, PI_ALT0); // set TXD pin to alternative mode 0 (TXD0)
    gpioSetMode(RXD, PI_ALT0); // set LED pin to alternative mode 0 (RXD0)
+   gpioSetMode(RST, PI_OUTPUT); // set LED pin to alternative mode 0 (RXD0)
    
    int uLCDhandle = serOpen(devtty, 9600, 0); // open a serial device at a specified baud rate
-   std::cout<<"serialHandle: "<< uLCDhandle << "\n\r";
    if((uLCDhandle == PI_NO_HANDLE)||(uLCDhandle == PI_SER_OPEN_FAILED)){
        std::cout <<"ERROR: PI_NO_HANDLE or PI_SER_OPEN_FAILED.\n\r";
        return -1;
    }
-   time_sleep(5.0);   
-   if( serClose(uLCDhandle)!= 0 ) {
-       std::cout <<"ERROR: PI_BAD_HANDLE.\n\r";
-       return -1;
-   }
 
-    while(1){
-	time_sleep(5.0);         
-   }
 
+   time_sleep(5.0);
+   serClose(uLCDhandle);
    gpioTerminate();
    return 0;
 }
